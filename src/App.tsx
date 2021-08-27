933f42fd073aa6f2f37218762ffba88dc86fd20e
john <lord1229@gmail.com>
2021-08-28 01:42:44 +0900

    voice record 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, message, Upload } from "antd";
+import { Button, Card, Image, Upload } from "antd";
+import { useVoiceRecorder } from "use-voice-recorder";
 
 function App() {
-  const props = {
-    beforeUpload: (file: any) => {
-      if (file.type !== "image/png") {
-        message.error(`${file.name} is not a png file`);
-      }
-      return file.type === "image/png" ? true : Upload.LIST_IGNORE;
-    },
-    onChange: (info: any) => {
-      console.log(info.fileList);
-    },
-  };
+  const [records, updateRecords] = useState<string[]>([]);
+  const { isRecording, stop, start } = useVoiceRecorder((data) => {
+    updateRecords([...records, window.URL.createObjectURL(data)]);
+  });
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Upload {...props}>
-          <Button>Upload png only</Button>
-        </Upload>
+        <Card style={{ marginTop: "20px", width: "500px" }}>
+          <Image width={"400px"} height={"400px"} src="" />
+          <br />
+          <Upload>
+            <Button>Upload Images</Button>
+          </Upload>
+        </Card>
+        <Card style={{ marginTop: "20px", width: "500px" }}>
+          <h1>Voice Record</h1>
+          <div>
+            <h3>On air: {isRecording ? "on" : "off"}</h3>
+            <button onClick={start}>Start</button>
+            <button onClick={stop}>Stop</button>
+          </div>
+          <div>
+            <h1>Records:</h1>
+            {records.map((data, idx) => (
+              <div key={idx}>
+                <audio src={data} controls preload={"metadata"} />
+              </div>
+            ))}
+          </div>
+        </Card>
       </header>
     </div>
   );
