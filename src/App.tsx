8625462fd007757f6ecf2ab47fd1573c45147a2e
john <lord1229@gmail.com>
2021-08-28 00:43:40 +0900

    image input 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button, message, Upload } from "antd";
 
 function App() {
+  const props = {
+    beforeUpload: (file: any) => {
+      if (file.type !== "image/png") {
+        message.error(`${file.name} is not a png file`);
+      }
+      return file.type === "image/png" ? true : Upload.LIST_IGNORE;
+    },
+    onChange: (info: any) => {
+      console.log(info.fileList);
+    },
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,8 +23,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <input type="file" accept="image/x-png,image/jpeg,image/gif" />
-        <button>Learn React</button>
+        <Upload {...props}>
+          <Button>Upload png only</Button>
+        </Upload>
       </header>
     </div>
   );
